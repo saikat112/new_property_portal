aad00eb5b4b0f9b314e47f7aedcbc87dc62db5e6
saikat112 <saikatmanna112@gmail.com>
2025-03-29 12:09:08 +0530

    dubg

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,6 +10,86 @@ async function main() {
         developer: "Fortune Park Housing",
         type: "2, 3, 4 BHK",
         location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Shree Heights",
+        developer: "Dream Royally LLP",
+        type: "2, 3 BHK",
+        location: "Chandannagar, Hooghly",
+        category: "newly_launched_property",
+        minPrice: "₹44.2 L",
+        maxPrice: "₹65.4 L",
+        imageUrl: "https://dummyimage.com/150x100/007bff/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "newly_launched_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
         category: "hot_property",
         minPrice: "₹37.0 L",
         maxPrice: "₹1.12 Cr",
@@ -24,7 +104,67 @@ async function main() {
         minPrice: "₹44.2 L",
         maxPrice: "₹65.4 L",
         imageUrl: "https://dummyimage.com/150x100/007bff/fff"
-      }
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "hot_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "hot_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "hot_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "hot_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "hot_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
+      {
+        title: "Fortune Heights",
+        developer: "Fortune Park Housing",
+        type: "2, 3, 4 BHK",
+        location: "Barasat, Kolkata",
+        category: "hot_property",
+        minPrice: "₹37.0 L",
+        maxPrice: "₹1.12 Cr",
+        imageUrl: "https://dummyimage.com/150x100/000/fff"
+      },
     ],
   });
 
